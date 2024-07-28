c379d522fc2ce41f775d15ecefcfa9dbb34c093a
Nischal876 <nischalaryal11@gmail.com>
2024-07-28 17:23:06 +0545

    Add custom validation messages for Customer entity - Added custom error message for type mismatch in freePasses field

--- src/main/java/com/example/validation/data_validation/controller/CustomerController.java
+++ src/main/java/com/example/validation/data_validation/controller/CustomerController.java
@@ -32,6 +32,11 @@ public class CustomerController {
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
 
         System.out.println("Last name: |"+ theCustomer.getLastName() + "|");
+
+        System.out.println("Binding results: " + theBindingResult.toString());
+
+        System.out.println("\n\n\n\n");
+
         if (theBindingResult.hasErrors()) {
             return "customer-form";
         }
