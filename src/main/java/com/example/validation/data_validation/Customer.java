904f5c49a665018e5f85d5f6c3862d3c9dcb936d
Nischal876 <nischalaryal11@gmail.com>
2024-07-28 15:08:52 +0545

    Added a Free passes field that does number range validation for the entered data

--- src/main/java/com/example/validation/data_validation/Customer.java
+++ src/main/java/com/example/validation/data_validation/Customer.java
@@ -1,5 +1,7 @@
 package com.example.validation.data_validation;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +11,11 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to 10")
+
+    private String freePasses;
 
     public String getFirstName() {
         return firstName;
@@ -24,5 +31,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(String freePasses) {
+        this.freePasses = freePasses;
     }
 }
