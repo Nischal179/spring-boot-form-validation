5253c849413f35cb5d8960a405818cff0882b98b
Nischal876 <nischalaryal11@gmail.com>
2024-07-28 15:48:48 +0545

    Added a new PostalCode input text field in the customer form - Applied regular expression validation that it requires 5 characters or digits

--- src/main/java/com/example/validation/data_validation/Customer.java
+++ src/main/java/com/example/validation/data_validation/Customer.java
@@ -1,9 +1,6 @@
 package com.example.validation.data_validation;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -14,8 +11,10 @@ public class Customer {
 
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be less than or equal to 10")
-
     private String freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -39,5 +38,13 @@ public class Customer {
 
     public void setFreePasses(String freePasses) {
         this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
