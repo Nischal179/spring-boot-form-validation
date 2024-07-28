f952515404bd186e3ccdec295ab2aea5e847c0bf
Nischal876 <nischalaryal11@gmail.com>
2024-07-28 11:15:37 +0545

    Initial commit

---
+++ src/test/java/com/example/validation/data_validation/DataValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.validation.data_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
